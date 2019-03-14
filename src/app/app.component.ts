fd9da8947bae4a0ab717f9a8acbb2f7dfa5e4835
Gonzalo Lucero <gonzigonz@users.noreply.github.com>
2019-03-14 12:16:17 -0700

    Init Ng app with Canvas working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'duck-hunt';
+export class AppComponent implements OnInit {
+  title = 'Duck Hunt';
+  @ViewChild('canvasScene') canvasRef: ElementRef;
+
+  ngOnInit(): void {
+    let canvas = this.canvasRef.nativeElement;
+    let ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
 }
