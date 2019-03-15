f2c383e9e3e651561746ba12b10f340b47f432fb
Gonzalo Lucero <gonzigonz@users.noreply.github.com>
2019-03-15 11:59:20 -0700

    Implemented Start, Pause & Restart

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -12,25 +12,45 @@ export class GameComponent implements OnInit, OnDestroy {
   // @Input('initGrassY') initGrassY: number;
   @ViewChild('canvasScene') canvasRef: ElementRef;
 
+  drawFuncLoaded: boolean;
   running: boolean;
   backGround: IGameObject;
   targets: IGameObject[];
   canvas: any;
-  level: LevelObject;
+  levelObject: LevelObject;
+  level: number;
 
   constructor(private ngZone: NgZone) {
+    this.running = false;
+    this.level = 1;
   }
 
   ngOnInit(): void {
     this.canvas = this.canvasRef.nativeElement;
     this.backGround = new GameBackGround(this.canvas.width, this.canvas.height);
-    this.level = new LevelObject(this.backGround.width, this.backGround.height);
-    this.targets = this.level.generateTargetsForLevel(3);
-    this.running = true;
-    this.ngZone.runOutsideAngular(() => this.draw());
+    this.levelObject = new LevelObject(this.backGround.width, this.backGround.height);
+    this.targets = this.levelObject.generateTargetsForLevel(this.level);
   }
   ngOnDestroy(): void {
     this.running = false;
+  }
+  startPause() {
+    if (this.running) {
+      this.running = false;
+      return;
+    }
+    this.running = true;
+    this.ngZone.runOutsideAngular(() => this.draw());
+  }
+  restart() {
+    this.level = 1;
+    this.running = false;
+    let ctx: CanvasRenderingContext2D = this.canvas.getContext('2d');
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.targets = this.levelObject.generateTargetsForLevel(this.level);
+    setTimeout(() => {
+      this.startPause();
+    }, 300)
   }
   draw() {
     // Check that we're still running.
