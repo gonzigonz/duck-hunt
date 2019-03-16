7cf71bb93f67a394b764633920e2a450681671a8
Gonzalo Lucero <gonzigonz@users.noreply.github.com>
2019-03-15 17:18:24 -0700

    Mallard update

--- src/app/model/targets/Mallard.ts
+++ src/app/model/targets/Mallard.ts
@@ -6,23 +6,43 @@ import { IFlightPath } from "./IFlightPath";
 export class Mallard extends GameTargetObject implements IGameObject {
     constructor(xBoundary: number, yBoundary: number) {
         super();
-        this.width = 40;
-        this.height = 30;
-        this.color = 'rgba(255, 255, 210, 0.85)';
+        this.width = 26;
+        this.height = 12;
         this.flightPath = new MarllardFlightPath(xBoundary, yBoundary);
         this.draw = this.drawFunc;
         this.checkIfHit = this.checkIfHitFunc;
     }
     drawFunc(ctx: CanvasRenderingContext2D) {
         // Draw your target object here
-        ctx.fillStyle = this.color
-        ctx.fillRect(this.pos.x, this.pos.y, this.width, this.height);
+
+        // body
+        ctx.beginPath();
+        ctx.ellipse(this.pos.x, this.pos.y, this.width, this.height, 0, 0, 2 * Math.PI);
+        ctx.closePath();
+        if (this.hit)
+            ctx.fillStyle = this.color;
+        else
+            ctx.fillStyle = 'rgba(155, 138, 97, 1)';
+        ctx.fill();
+
+        // head
+        let headWidth = this.width * 0.4;
+        let headHeight = this.height * 0.5;
+        ctx.beginPath();
+        ctx.fill();
+        ctx.ellipse(this.pos.x + this.width * 1.3, this.pos.y -3, headWidth, headHeight, 0, 0, 2 * Math.PI);
+        ctx.closePath();
+        if (this.hit)
+            ctx.fillStyle = this.color;
+        else
+            ctx.fillStyle = 'rgba(22, 205, 101, 1';
+        ctx.fill();
     }
     checkIfHitFunc(lastClick: GameObjectPosition) {
         // Define your hit criteria here
         let lc = lastClick;
-        let isXCorrect = lc.x > this.pos.x && lc.x < this.pos.x + this.width;
-        let isYCorrect = lc.y > this.pos.y && lc.y < this.pos.y + this.height;
+        let isXCorrect = lc.x > this.pos.x - this.width/2 && lc.x < this.pos.x + this.width/2;
+        let isYCorrect = lc.y > this.pos.y - this.height && lc.y < this.pos.y + this.height/2;
         return isXCorrect && isYCorrect
     }
 }
